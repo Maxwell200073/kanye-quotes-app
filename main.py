76ef76422746f4fabcec88b51db6c2deb16e2cf4
Maxwell200073 <91772625+Maxwell200073@users.noreply.github.com>
2021-12-30 21:09:07 -0600

    added raise for exceptions

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ FONT = ("Open Sans", 20)
 
 def get_quote():
     response = requests.get('https://api.kanye.rest/')
+    response.raise_for_status()
     quote = response.json()['quote']
     canvas.itemconfig(quote_text, text=quote)
 
